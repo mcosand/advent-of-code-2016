eea9ebaf0945c194d4c92c88b8e734742bd1df89
Matt Cosand <matt@cosand.net>
2016-12-04 15:23:20 -0800

    day 3 part 2

--- day-03/Program.cs
+++ day-03/Program.cs
@@ -13,17 +13,35 @@ namespace day_03
     {
       var groups = File.ReadAllLines(args.Length > 0 ? args[0] : "input.txt");
       var possible = 0;
-      foreach (var group in groups)
+      for (var i = 0; i < groups.Length; i += 3)
       {
-        var sides = group.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-          .Select(f => int.Parse(f))
-          .OrderBy(f => f)
-          .ToArray();
+        var set = new[]
+        {
+          ReadLine(groups[i]),
+          ReadLine(groups[i + 1]),
+          ReadLine(groups[i + 2])
+        };
 
-        if (sides[0] <= 0) continue;
-        if (sides[0] + sides[1] > sides[2]) possible++;
+        for (var j = 0; j < 3; j++)
+          possible += TestGroup(possible, set, j);
+
       }
+
       Console.WriteLine(possible);
+    }
+
+    private static int TestGroup(int possible, int[][] set, int j)
+    {
+      var sides = new[] { set[0][j], set[1][j], set[2][j] }.OrderBy(f => f).ToArray();
+      Console.WriteLine(string.Format("{0}, {1} {2}", sides[0], sides[1], sides[2]));
+      return (sides[0] >= 0 && sides[0] + sides[1] > sides[2]) ? 1 : 0;
+    }
+
+    private static int[] ReadLine(string group)
+    {
+      return group.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(f => int.Parse(f))
+        .ToArray();
     }
   }
 }
