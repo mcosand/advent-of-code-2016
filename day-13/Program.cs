7cedea412fad7d160494a3f2008b5d384bd6f065
Matt Cosand <matt@cosand.net>
2016-12-12 23:26:50 -0800

    day 13 part 2

--- day-13/Program.cs
+++ day-13/Program.cs
@@ -15,7 +15,8 @@ namespace day_13
     private readonly uint _seed = 1364;
     private readonly uint _goalX = 31;
     private readonly uint _goalY = 39;
-    private int bestPath = int.MaxValue;
+    private int bestPath = 50 + 1;
+    private int count = 0;
 
     static void Main(string[] args)
     {
@@ -29,7 +30,7 @@ namespace day_13
       Console.ForegroundColor = foreColor;
       Console.WriteLine();
       Console.WriteLine();
-      Console.WriteLine("Length: " + bestPath.ToString());
+      Console.WriteLine("Spots: " + count.ToString());
     }
 
     private void Expand(uint x, uint y, int distance)
@@ -42,8 +43,8 @@ namespace day_13
       Console.ForegroundColor = (ConsoleColor)(((distance / 10 + 2) % 15) + 1) ;
       if (x == _goalX && y == _goalY)
       {
-        if (distance <= bestPath) bestPath = distance - 1;
-        return;
+       // if (distance <= bestPath) bestPath = distance - 1;
+       // return;
       }
 
       if (spaces[x] == null) spaces[x] = new int[1000];
@@ -51,16 +52,17 @@ namespace day_13
       if (NumberOfSetBits((x * x + 3 * x + 2 * x * y + y + y * y) +_seed) % 2 == 1 && !(x == 1 && y == 1))
       {
         spaces[x][y] = -1;
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
         Console.Write('#');
         return;
       }
       if (spaces[x][y] != 0 && distance >= spaces[x][y])
         return;
 
-
+      if (spaces[x][y] == 0) count++;
       spaces[x][y] = distance;
       Console.Write(distance % 10);
-     
+
       if (x > 0) Expand(x - 1, y, distance + 1);
       if (y > 0) Expand(x, y - 1, distance + 1);
       Expand(x + 1, y, distance + 1);
