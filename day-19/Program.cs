12fff37fd5acb7e32a55b1e9687e8433edd21b80
Matt Cosand <matt@cosand.net>
2016-12-18 21:47:28 -0800

    day 19 part 2. From 234th for part 1 to 33rd for 2! Yay linked lists!

--- day-19/Program.cs
+++ day-19/Program.cs
@@ -11,6 +11,7 @@ namespace day_19
     public int id { get; set; }
     public int presents { get; set; }
     public Elf nextElf { get; set; }
+    public Elf prevElf { get; set; }
   }
 
 
@@ -19,30 +20,43 @@ namespace day_19
     static void Main(string[] args)
     {
       int size = 3017957;
-      //size = 5;
+      size = 5;
 
       Elf root = new Elf { id = 1, presents = 1 };
       Elf elf = root;
+      Elf target = null;
+
+      // build doubly-linked list so we can repair the ring when we remove the target
       for (int i=1;i<size+1;i++)
       {
-        elf.nextElf = (i == size) ? root : new Elf { id = i + 1, presents = 1 };
-   //     Console.WriteLine(elf.id + " " + elf.nextElf.id);
+        elf.nextElf = (i == size) ? root : new Elf { id = i + 1, presents = 1, prevElf = elf };
         elf = elf.nextElf;
+        if (i == size / 2) target = elf;
       }
-      //elf.nextElf = root;
+      root.prevElf = elf;
+
+      int count = size;
+
+      // We have two cursors on the ring, opposite each other.
+      // March the two cursors around the shrinking ring.
 
       Console.WriteLine("built");
       while (elf.nextElf != elf)
       {
-    //    Console.WriteLine("{0} steals {1}'s {2} presents", elf.id, elf.nextElf.id, elf.nextElf.presents);
-        elf.presents += elf.nextElf.presents;
-        elf.nextElf = elf.nextElf.nextElf;
-    //    Console.WriteLine("{0} is now next to {1}", elf.id, elf.nextElf.id);
+    //    Console.WriteLine("{0} steals {1}'s {2} presents", elf.id, target.id, target.presents);
+        elf.presents += target.presents;
+
+
+        target.prevElf.nextElf = target.nextElf;
+        target.nextElf.prevElf = target.prevElf;
+        target = count % 2 == 1 ? target.nextElf.nextElf : target.nextElf;
+        count--;
+
         elf = elf.nextElf;
       }
-      
-      Console.WriteLine(elf.id);
+
       int answer = elf.id;
+      Console.WriteLine(answer);
     }
   }
 }
